0b97683282515433a998eeb1da1c6ae2978ec69f
DK\psk <psk@sitecore.net>
2018-06-27 13:58:37 +0300

    Init

---
+++ src/Sitecore.Support.Diagnostics.FetchIndex/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.Diagnostics.FetchIndex")]
+[assembly: AssemblyProduct("Sitecore.Support.Diagnostics.FetchIndex")]
+[assembly: ComVisible(false)]
