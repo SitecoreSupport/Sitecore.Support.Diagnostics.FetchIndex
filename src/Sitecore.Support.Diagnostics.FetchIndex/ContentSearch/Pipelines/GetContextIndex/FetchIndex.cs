05f349d0c54a4cef4ef0113a69cb7f242ec6baf7
DK\psk <psk@sitecore.net>
2018-06-27 14:42:12 +0300

    Add default index resolution logic

---
+++ src/Sitecore.Support.Diagnostics.FetchIndex/ContentSearch/Pipelines/GetContextIndex/FetchIndex.cs
@@ -0,0 +1,140 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Sitecore.Caching.Generics;
+using Sitecore.ContentSearch;
+using Sitecore.ContentSearch.Abstractions;
+using Sitecore.ContentSearch.Pipelines.GetContextIndex;
+using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
+using Sitecore.Reflection;
+using Sitecore.SecurityModel;
+
+namespace Sitecore.Support.ContentSearch.Pipelines.GetContextIndex
+{
+  /// <summary>
+  /// The <see cref="FetchIndex"/> class.
+  /// </summary>
+  public class FetchIndex : GetContextIndexProcessor
+  {
+    private readonly ISettings settings;
+
+    public FetchIndex()
+    {
+      this.settings = ContentSearchManager.Locator.GetInstance<ISettings>();
+    }
+
+    internal FetchIndex(ISettings settings)
+    {
+      this.settings = settings;
+    }
+
+    /// <summary>Processes the specified args.</summary>
+    /// <param name="args">The args.</param>
+    public override void Process(GetContextIndexArgs args)
+    {
+      if (args == null)
+      {
+        return;
+      }
+
+      if (args.Result != null)
+      {
+        return;
+      }
+
+      args.Result = this.GetContextIndex(args.Indexable, args);
+    }
+
+    /// <summary>Gets the index of the context.</summary>
+    /// <param name="indexable">The indexable.</param>
+    /// <param name="args">The args.</param>
+    /// <returns>The result.</returns>
+    protected virtual string GetContextIndex(IIndexable indexable, GetContextIndexArgs args)
+    {
+      if (indexable == null)
+      {
+        return null;
+      }
+
+      var indexes = from searchIndex in ContentSearchManager.Indexes
+                    from providerCrawler in searchIndex.Crawlers
+                    where !providerCrawler.IsExcludedFromIndex(indexable)
+                    select searchIndex;
+
+      if (!indexes.Any())
+      {
+        indexes = this.FindIndexesRelatedToIndexable(args.Indexable, ContentSearchManager.Indexes);
+      }
+
+      // ReSharper disable once PossibleMultipleEnumeration
+      var rankedIndexes = RankContextIndexes(indexes, indexable);
+
+      var searchIndices = rankedIndexes as Tuple<ISearchIndex, int>[] ?? rankedIndexes.ToArray();
+
+      // No crawlers index this item..
+      if (!searchIndices.Any())
+      {
+        Log.Error(string.Format("There is no appropriate index for {0} - {1}. You have to add an index crawler that will cover this item", indexable.AbsolutePath, indexable.Id), this);
+        return null;
+      }
+
+      // If only one matches then use that..
+      if (searchIndices.Count() == 1)
+      {
+        return searchIndices.First().First.Name;
+      }
+
+      if (searchIndices.First().Second < searchIndices.Skip(1).First().Second)
+      {
+        return searchIndices.First().First.Name;
+      }
+
+      // Get default type from setting ..
+      var defaultTypeString = this.settings.GetSetting("ContentSearch.DefaultIndexType", "");
+      var defaultType = ReflectionUtil.GetTypeInfo(defaultTypeString);
+
+      //If we cant evaluate this type then return first ..
+      if (defaultType == null)
+      {
+        return searchIndices[0].First.Name;
+      }
+
+      // Return the one that matches the default type ..
+      var matchedIndex = searchIndices.Where(i => i.First.GetType() == defaultType).OrderBy(i => i.First.Name).ToArray();
+      return matchedIndex.Any() ? matchedIndex[0].First.Name : searchIndices[0].First.Name;
+    }
+
+    protected virtual IEnumerable<ISearchIndex> FindIndexesRelatedToIndexable(IIndexable indexable, IEnumerable<ISearchIndex> indexes)
+    {
+      SitecoreIndexableItem sitecoreIndexableItem = indexable as SitecoreIndexableItem;
+
+      if (sitecoreIndexableItem == null)
+      {
+        return new List<ISearchIndex>();
+      }
+
+      Item item = sitecoreIndexableItem.Item;
+
+      using (new SecurityDisabler())
+      {
+        using (new WriteCachesDisabler())
+        {
+          return indexes
+              .Where(i => i.Crawlers.OfType<SitecoreItemCrawler>()
+                  .Where(crawler => crawler.RootItem != null)
+                  .Any(crawler => item.Database.Name.Equals(crawler.Database, System.StringComparison.InvariantCultureIgnoreCase)
+                                  && item.Paths.LongID.StartsWith(crawler.RootItem.Paths.LongID, System.StringComparison.InvariantCulture)));
+        }
+      }
+    }
+
+    /// <summary>Ranks the context indexes.</summary>
+    /// <param name="indexes">The indexes.</param>
+    /// <param name="indexable">The indexable.</param>
+    /// <returns>Ranked indexes.</returns>
+    protected virtual IEnumerable<Tuple<ISearchIndex, int>> RankContextIndexes(IEnumerable<ISearchIndex> indexes, IIndexable indexable)
+    {
+      return indexes.Distinct().Select(i => Tuple.New(i, (i is IContextIndexRankable) ? ((IContextIndexRankable)i).GetContextIndexRanking(indexable) : int.MaxValue)).OrderBy(i => i.Second);
+    }
+  }
+}
--- src/Sitecore.Support.Diagnostics.FetchIndex/Properties/AssemblyInfo.cs
+++ src/Sitecore.Support.Diagnostics.FetchIndex/Properties/AssemblyInfo.cs
@@ -4,3 +4,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Sitecore.Support.Diagnostics.FetchIndex")]
 [assembly: AssemblyProduct("Sitecore.Support.Diagnostics.FetchIndex")]
 [assembly: ComVisible(false)]
+[assembly: AssemblyDescription("Provide additional logging for the process of search index resolution")]
+
